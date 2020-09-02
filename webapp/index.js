5be4bda555b69ff533e3d7d8810a8c1336fa872d
yohamc <yohanamonforte629@hotmail.com>
2020-09-02 00:47:42 +0000

    123

--- webapp/index.js
+++ webapp/index.js
@@ -1,6 +1,15 @@
-sap.ui.define([],
-	function () {
-		/*eslint-disable no-alert */
-		alert("UI cargado");
-		/*eslint-enable no-alert */
+sap.ui.define([
+		"sap/m/Text"
+	],
+
+	// function () {
+	// 	/*eslint-disable no-alert */
+	// 	alert("UI cargado");
+	// 	/*eslint-enable no-alert */
+	// });
+
+	function (Text) {
+		new Text({
+			text: "Hello Word!"
+		}).placeAt("content");
 	});
