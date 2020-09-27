5d1fa123e51fd9548441d11cc471ec8c12c10d28
yohamc <yohanamonforte629@hotmail.com>
2020-09-27 21:01:44 +0000

    3

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,10 +1,22 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/m/MessageToast"
-], function (Controller, MessageToast) {
+	"sap/m/MessageToast",
+	"sap/ui/model/json/JSONModel"
+], function (Controller, MessageToast, JSONModel) {
 	"use strict";
 
 	return Controller.extend("LogaliUI5/HolaMundo.controller.App", {
+
+		onInit: function () {
+			//Set model on View
+			var oData = {
+				recipient: {
+					name: "Word"
+				}
+			};
+			var oModel = new JSONModel(oData);
+			this.getView().setModel(oModel);
+		},
 
 		onShowHello: function () {
 			MessageToast.show("Hello Word");
