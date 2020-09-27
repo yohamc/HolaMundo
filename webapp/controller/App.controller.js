5f85478a2b817ec54182909e5807e6fcead9d38a
yohamc <yohanamonforte629@hotmail.com>
2020-09-27 23:12:52 +0000

    g

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -21,7 +21,12 @@ sap.ui.define([
 		},
 
 		onShowHello: function () {
-			MessageToast.show("Hello Word");
+			//read text from i18n / model
+			var sHello = this.getView().getModel("i18n").getResourceBundle().getText("sayHello");
+			var sName = this.getView().getModel().getProperty("/recipient/name");
+			var sMsg = sHello.concat(" ").concat(sName);
+			
+			MessageToast.show(sMsg);
 		}
 	});
 });
